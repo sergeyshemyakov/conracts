f69a119c72cbd150d7633b4d1b239d43b8ac2dc1
Sergey Shemyakov <sergey.shemyakov@lightcurve.io>
2024-10-23 15:08:52 +0200

    Finished testing cross chain wallet

--- CCBatcher/cross_chain_batcher/script/CrossChainWallet.s.sol
+++ CCBatcher/cross_chain_batcher/script/CrossChainWallet.s.sol
@@ -26,7 +26,8 @@ contract CrossChainWalletScript is Script {
 // cast send 0x6B48c8134760B85637872D3dedD6DdfDa467037D "sendSetTestFlag(uint256)()" 902 --rpc-url http://127.0.0.1:9545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
 // cast call 0x6B48c8134760B85637872D3dedD6DdfDa467037D "lastRelayer()(address)" --rpc-url http://127.0.0.1:9546
 // cast send 0x420beeF000000000000000000000000000000001 "mint(address _to, uint256 _amount)"  0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 1000  --rpc-url http://127.0.0.1:9545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
-// cast send 0x420beeF000000000000000000000000000000001 "transfer(address,uint256)(bool)" 0x6B48c8134760B85637872D3dedD6DdfDa467037D 1000 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --rpc-url http://127.0.0.1:9545
+// cast send 0x420beeF000000000000000000000000000000001 "transfer(address,uint256)(bool)" 0x6B48c8134760B85637872D3dedD6DdfDa467037D 500 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --rpc-url http://127.0.0.1:9545
 // cast call 0x420beeF000000000000000000000000000000001 "balanceOf(address)(uint256)" 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 --rpc-url http://127.0.0.1:9545
-
 // cast send 0x6B48c8134760B85637872D3dedD6DdfDa467037D "sendSetTestFlagWithTip(uint256,address,uint256)()" 902 0x420beeF000000000000000000000000000000001 250 --rpc-url http://127.0.0.1:9545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+// cast call 0x6B48c8134760B85637872D3dedD6DdfDa467037D "testFlag()(uint256)" --rpc-url http://127.0.0.1:9545
+// cast call 0x420beeF000000000000000000000000000000001 "balanceOf(address)(uint256)" 0xa0Ee7A142d267C1f36714E4a8F75612F20a79720 --rpc-url http://127.0.0.1:9546
